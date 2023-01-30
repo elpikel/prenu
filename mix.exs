36d056d49f9732e35d6e5b099bb3edfc0bc4dbd9
Arkadiusz Plichta <el.pikel@gmail.com>
2023-01-30 20:30:58 +0100

    dialyzer

--- mix.exs
+++ mix.exs
@@ -9,7 +9,10 @@ defmodule Prenu.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -52,7 +55,8 @@ defmodule Prenu.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.6"}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.2", only: [:dev], runtime: false}
     ]
   end
 
