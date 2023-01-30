4f0e2531d88f518969c83171142d10c1b1ecddc6
Arkadiusz Plichta <el.pikel@gmail.com>
2023-01-30 20:41:41 +0100

    sobelow

--- mix.exs
+++ mix.exs
@@ -56,7 +56,8 @@ defmodule Prenu.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.2", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.2", only: [:dev], runtime: false},
+      {:sobelow, "~> 0.11.1", only: :dev}
     ]
   end
 
